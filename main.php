32c2dfa8a964948698e0e8038132c71ad8b92573
ryoookaze <32873309+ryoookaze@users.noreply.github.com>
2019-03-23 22:46:07 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script 
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+        crossorigin="anonymous">
+</script>
+<script 
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+        crossorigin="anonymous">
+</script>
+<script 
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+        crossorigin="anonymous">
+</script>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>Homepage</title>
+ <style>
+   .card-title {
+      font-size: 30px;
+   }
+ </style>
+</head>
+<body>
+<div class="container-fluid">
+   <h1>My Profile</h1>
+ </div>
+
+ <div class="container">
+   <div class="row justify-content-md-center">
+   <div class="card bg-light mb-3" style="max-width:600px;">
+   <div class="row no-gutters">
+      <div class="col-md-4">
+         <img src="" class="card-img" alt="...">
+      </div>
+      <div class="col-md-8">
+         <div class="text-center">
+         <div class="card-body">
+            <h5 class="card-title">Akbar Firmansyah</h5>
+            <p class="card-title" style="font-size:20px;">Magelang, Jawa Tengah</p>
+            <p class="card-text"><small class="text-muted">Universitas Negeri Malang</small></p>
+         </div>
+         </div>
+      </div>
+   </div>
+   </div>
+ </div>
+ </div>
+   <!-- Card for Profile -->
+ <div class="container">
+   <div class="row justify-content-md-center">
+      <div class="row">
+         <div class="col">
+            1 of 3
+         </div>
+      </div>
+   </div>
+ </div>
+</body>
+</html>
