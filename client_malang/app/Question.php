05621229f1eccd5f73cce6d186e3edde6460bc63
adyg <yogamuris@gmail.com>
2019-04-13 21:07:39 +0700

    resolve conflict

--- client_malang/app/Question.php
+++ client_malang/app/Question.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
 {
-<<<<<<< HEAD
     use SoftDeletes;
 
     protected $fillable = [
@@ -18,9 +17,7 @@ class Question extends Model
         return $this->belongsToMany('\App\Submission');
     }
 
-=======
     public function quiz() {
         return $this->belongsTo('App\Quiz', 'id_quiz');
     }
->>>>>>> a3676adc1250799ae8a33ac1646ab01db9772082
 }
